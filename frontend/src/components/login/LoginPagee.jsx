8e4a23b08d83c5454b184fcd2293f841d8b7973e
PREMKUMAR5519 <premkumarananthan1@gmail.com>
2024-09-11 17:15:58 +0530

    final

--- frontend/src/components/login/LoginPagee.jsx
+++ frontend/src/components/login/LoginPagee.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import '../../styles/LoginPagee.css'
+import profile from '../../assets/profile.png'
+import { IoMailSharp } from "react-icons/io5";
+import { IoIosLock } from "react-icons/io";
+import backvideo from '../../images/backvideo.mp4'
+
 function LoginPagee() {
   return (
     <div className='loginpageemain'>
-hi
+       <video loop muted autoPlay>
+                <source src={backvideo} type="video/mp4" />
+              </video>
+      
+        <div className='loginpagee_div1'>
+        <div className='loginpagee_div11'>
+          <img src={profile} alt="" />
+          <h1>CUSTOMER LOGIN</h1>
+          <div className='loginpagee_input'>
+            <input placeholder='Enter Email' type="text" />
+            <p><IoMailSharp/></p>
+
+          </div>
+          <div className='loginpagee_input'>
+            <input  placeholder='Enter Password' type="password" />
+            <p><IoIosLock/></p>
+
+          </div>
+          <button>LOGIN</button>
+          <h3>New User? Create Account</h3>
+
+      </div>
+        </div>
+ 
     </div>
   )
 }
