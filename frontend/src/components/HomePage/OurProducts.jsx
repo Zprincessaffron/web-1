649a349c2d026ed1d0944b12d75394e7e917393c
PREMKUMAR5519 <premkumarananthan1@gmail.com>
2024-09-11 13:44:51 +0530

    fin

--- frontend/src/components/HomePage/OurProducts.jsx
+++ frontend/src/components/HomePage/OurProducts.jsx
@@ -4,15 +4,15 @@ import ourproductback from '../../images/ourproductsback.jpeg'
 import ourproductback1 from '../../images/homepageback2.jpg'
 
 import { useNavigate } from 'react-router-dom'
+import Arrowbtn from '../button/Arrowbtn'
 function OurProducts() {
   const navigate=useNavigate()
   return (
     <div className='unveil_main' style={{backgroundImage:`url(${ourproductback})`}}>
       <div className='unveil_div1'>
         <h1>OUR PRODUCTS</h1>
-        <button onClick={()=>navigate('/productpage')}>DISCOVER NOW</button>
-
-      </div>
+        <Arrowbtn linkFromMain='/productpage' onMouseEnter={()=>{setIsMouse(false)}} onMouseLeave={()=>{setIsMouse(true)}} name="DISCOVER NOW"/>
+        </div>
        </div>
   )
 }
