d392c0ec56edec985b4fb80aa700d60e59159939
PREMKUMAR5519 <premkumarananthan1@gmail.com>
2024-09-17 15:59:37 +0530

    recent-c

--- frontend/src/components/product/ProductDetails.jsx
+++ frontend/src/components/product/ProductDetails.jsx
@@ -263,29 +263,29 @@ console.log(btnDissable);
         <div className="product-details">
            {isMobile?(
                      
-
+          
     <Slider className="pd_slider" {...settings}>
 
-    <div>
-      <h3>1</h3>
-    </div>
-    <div>
-      <h3>2</h3>
-    </div>
-    <div>
-      <h3>3</h3>
-    </div>
-    <div>
-      <h3>4</h3>
-    </div>
-    <div>
-      <h3>5</h3>
-    </div>
-    <div>
-      <h3>6</h3>
-    </div>
-  </Slider>
-  
+<div>
+  <h3>1</h3>
+</div>
+<div>
+  <h3>2</h3>
+</div>
+<div>
+  <h3>3</h3>
+</div>
+<div>
+  <h3>4</h3>
+</div>
+<div>
+  <h3>5</h3>
+</div>
+<div>
+  <h3>6</h3>
+</div>
+</Slider>
+
 
 
            ):(
